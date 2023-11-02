c645b151259869d41bcc4b745f868572228adf97
Charles Perron <charlis70@hotmail.ca>
2023-11-02 11:53:08 -0400

    Ajout activity connexion

---
+++ tp3/app/src/main/java/com/example/jeudepart/ConnexionActivity.java
@@ -0,0 +1,58 @@
+package com.example.jeudepart;
+
+import android.os.Bundle;
+import android.text.Editable;
+import android.text.TextWatcher;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+
+public class ConnexionActivity extends AppCompatActivity {
+    private EditText emailInput;
+    private Button buttonConnection;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_conexion);
+        initEditField();
+    }
+
+    private void initEditField() {
+
+        // EMAIL INPUT
+        emailInput = findViewById(R.id.emailEdit);
+        emailInput.addTextChangedListener(new TextWatcher() {
+            @Override
+            public void beforeTextChanged(CharSequence s, int start, int count, int after) {
+            }
+
+            @Override
+            public void onTextChanged(CharSequence s, int start, int before, int count) {
+
+                if (!isValidate(emailInput.getText().toString())) {
+                    emailInput.setError("Addresse Courriel non valide");
+                  //  buttonConnection.setEnabled(false);
+                }
+
+            }
+
+            @Override
+            public void afterTextChanged(Editable s) {
+            }
+        });
+    }
+    private boolean isValidate(String email) {
+        Pattern VALID_EMAIL_ADDRESS_REGEX = Pattern.compile("^[A-Z0-9._%+-]+@[A-Z0-9.-]", Pattern.CASE_INSENSITIVE);
+        Matcher matcher = VALID_EMAIL_ADDRESS_REGEX.matcher(email);
+        return matcher.find();
+    }
+    public void connectionOnClick(View view) {
+
+    }
+}
--- tp3/app/src/main/java/com/example/jeudepart/MainActivity.java
+++ tp3/app/src/main/java/com/example/jeudepart/MainActivity.java
@@ -15,8 +15,9 @@ import android.widget.TextView;
 import java.text.DateFormat;
 import java.text.NumberFormat;
 import java.util.Date;
+
 public class MainActivity extends AppCompatActivity {
-    //
+
     private EditText txtNumber;
     private Button btnCompare;
     private TextView lblResult;
@@ -31,52 +32,52 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        txtNumber = (EditText) findViewById( R.id.txtNumber );
-        btnCompare = (Button) findViewById( R.id.btnCompare );
-        lblResult = (TextView) findViewById( R.id.lblResult );
-        pgbScore = (ProgressBar) findViewById( R.id.pgbScore );
-        lblHistory = (TextView) findViewById( R.id.lblHistory );
+        txtNumber = (EditText) findViewById(R.id.txtNumber);
+        btnCompare = (Button) findViewById(R.id.btnCompare);
+        lblResult = (TextView) findViewById(R.id.lblResult);
+        pgbScore = (ProgressBar) findViewById(R.id.pgbScore);
+        lblHistory = (TextView) findViewById(R.id.lblHistory);
 
-        btnCompare.setOnClickListener( btnCompareListener );
+        btnCompare.setOnClickListener(btnCompareListener);
 
         init();
 
         NumberFormat formatter = NumberFormat.getCurrencyInstance();
         double price = 1_000_000.01;
-        Log.i( "DEBUG", formatter.format( price ) );
+        Log.i("DEBUG", formatter.format(price));
 
         DateFormat dataFormatter = DateFormat.getDateTimeInstance();
-        Log.i( "DEBUG", dataFormatter.format( new Date() ) );
+        Log.i("DEBUG", dataFormatter.format(new Date()));
     }
 
     private void init() {
         score = 0;
         searchedValue = 1 + (int) (Math.random() * 100);
-        Log.i( "DEBUG", "Searched value : " + searchedValue );
+        Log.i("DEBUG", "Searched value : " + searchedValue);
 
-        txtNumber.setText( "" );
-        pgbScore.setProgress( score );
-        lblResult.setText( "" );
-        lblHistory.setText( "" );
+        txtNumber.setText("");
+        pgbScore.setProgress(score);
+        lblResult.setText("");
+        lblHistory.setText("");
 
         txtNumber.requestFocus();
     }
 
     private void congratulations() {
-        lblResult.setText( R.string.strCongratulations );
+        lblResult.setText(R.string.strCongratulations);
 
-        AlertDialog retryAlert = new AlertDialog.Builder( this ).create();
-        retryAlert.setTitle( R.string.app_name );
-        retryAlert.setMessage( getString(R.string.strMessage, score ) );
+        AlertDialog retryAlert = new AlertDialog.Builder(this).create();
+        retryAlert.setTitle(R.string.app_name);
+        retryAlert.setMessage(getString(R.string.strMessage, score));
 
-        retryAlert.setButton( AlertDialog.BUTTON_POSITIVE, getString(R.string.strYes), new AlertDialog.OnClickListener() {
+        retryAlert.setButton(AlertDialog.BUTTON_POSITIVE, getString(R.string.strYes), new AlertDialog.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 init();
             }
         });
 
-        retryAlert.setButton( AlertDialog.BUTTON_NEGATIVE, getString(R.string.strNo), new AlertDialog.OnClickListener() {
+        retryAlert.setButton(AlertDialog.BUTTON_NEGATIVE, getString(R.string.strNo), new AlertDialog.OnClickListener() {
             @Override
             public void onClick(DialogInterface dialog, int which) {
                 finish();
@@ -89,25 +90,25 @@ public class MainActivity extends AppCompatActivity {
     private View.OnClickListener btnCompareListener = new View.OnClickListener() {
         @Override
         public void onClick(View v) {
-            Log.i( "DEBUG", "Button clicked" );
+            Log.i("DEBUG", "Button clicked");
 
             String strNumber = txtNumber.getText().toString();
-            if ( strNumber.equals( "" ) ) return;
+            if (strNumber.equals("")) return;
 
-            int enteredValue = Integer.parseInt( strNumber );
-            lblHistory.append( strNumber + "\r\n" );
-            pgbScore.incrementProgressBy( 1 );
+            int enteredValue = Integer.parseInt(strNumber);
+            lblHistory.append(strNumber + "\r\n");
+            pgbScore.incrementProgressBy(1);
             score++;
 
-            if ( enteredValue == searchedValue ) {
+            if (enteredValue == searchedValue) {
                 congratulations();
-            } else if ( enteredValue < searchedValue ) {
-                lblResult.setText( R.string.strGreater );
+            } else if (enteredValue < searchedValue) {
+                lblResult.setText(R.string.strGreater);
             } else {
-                lblResult.setText( R.string.strLower );
+                lblResult.setText(R.string.strLower);
             }
 
-            txtNumber.setText( "" );
+            txtNumber.setText("");
             txtNumber.requestFocus();
 
         }
